1669d56e75cb7e5728979f4bd9beb9a5b991b8d4
rymate1234 <supermarioryan@gmail.com>
2017-08-20 10:52:58 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dotnet_irc_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IrcMessage msg = new IrcMessage(":james000-!~james000_@ns354110.ip-91-121-101.eu PRIVMSG #rymate :OUTRAAAGGEEEEE");
+
+            Console.WriteLine(msg.TrailMessage.TrailingContent);
+            
+        }
+    }
+}
