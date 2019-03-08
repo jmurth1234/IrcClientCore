bb7fe35496c246ada553ab3d93754f99b9a127f5
Ryan <supermarioryan@gmail.com>
2019-03-08 08:01:54 +0000

    Adds custom message hash code

--- IrcClientCore/Message.cs
+++ IrcClientCore/Message.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace IrcClientCore
@@ -56,6 +57,28 @@ namespace IrcClientCore
         public MessageType Type { get; set; }
 
         public int MessageHash => GetHashCode();
+
+        public override bool Equals(object obj)
+        {
+            var message = obj as Message;
+            return message != null &&
+                   Date == message.Date &&
+                   User == message.User &&
+                   Channel == message.Channel &&
+                   Text == message.Text &&
+                   Type == message.Type;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = 1712867738;
+            hashCode = hashCode * -1521134295 + Date.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(User);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Channel);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Text);
+            hashCode = hashCode * -1521134295 + Type.GetHashCode();
+            return hashCode;
+        }
     }
 
     public enum MessageType
